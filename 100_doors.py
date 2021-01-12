a213bc3d8f5d1d948d004e8143364f5ad372d5a6
dsynkd <darrensadr@gmail.com>
2021-01-12 15:49:57 -0600

    .

--- 100_doors.py
+++
@@ -1,9 +0,0 @@
-from typing import List
-
-def hundred_doors(doors: List[bool]) -> List[bool]:
-    for i in range(0,100):
-        for j in range(0,100,i+1):
-            doors[j] = not doors[j]
-    print(doors)
-
-hundred_doors([False for i in range(0,100)])
